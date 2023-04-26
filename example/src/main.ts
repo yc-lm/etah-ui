a59b205e30fc730775f3d66070aa8a0b70a8f99a
ycc <845837723@qq.com>
2023-04-26 14:12:59 +0800

    ci(组件库): 引入样式

--- example/src/main.ts
+++ example/src/main.ts
@@ -3,15 +3,13 @@ import App from './App.vue';
 import { setupRouter } from './router';
 // ant-design样式
 import 'ant-design-vue/dist/antd.variable.min.css';
-// 公共样式库
-import 'etah-styles/index.scss';
-//import './styles/index.scss';
-/*import test from '@packages/index';*/
+import '@/common/styles/index.scss';
+import test from '@packages/index';
 
 function bootstrap() {
 	const app = createApp(App);
 	setupRouter(app);
-	//test.install(app);
+	test.install(app);
 	app.mount('#app');
 }
 
--- example/src/views/home/homeIndex.vue
+++ example/src/views/home/homeIndex.vue
@@ -1,12 +1,8 @@
 <template>
 	home -index 11111333336666555577777
 	<test-button></test-button>
-	<!--	<a-button type="primary">Primary Button</a-button>-->
 	<divider>dfadsfads</divider>
 </template>
 <script lang="ts" setup>
 import { Divider } from 'ant-design-vue';
-import TestButton from '@/lib/TestButton/index';
-//import TestButton from '@packages/TestButton';
-//console.log(TestButton);
 </script>
